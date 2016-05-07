eedf158762a5dfe20a86475ca2cc85a3499760c3
Miguel Coronel <m.coronel.seg@gmail.com>
2016-05-07 02:39:01 -0500

    Add solved or not solved legend

--- dev/puzzle/puzzle.component.ts
+++ dev/puzzle/puzzle.component.ts
@@ -3,32 +3,7 @@ import {OnInit} from 'angular2/core';
 
 @Component({
   selector: 'my-puzzle',
-  template: `
-      <section class="setup">
-        <h2>Game Setup</h2>
-        Enter your name please:
-        <input type="text" #name (keyup)="0"/>
-      </section>
-      <section 
-      [ngClass]="{
-        puzzle: true,
-        solved: switch1.value == switch1Number && switch2.value == switch2Number && switch3.value == switch3Number && switch4.value == switch4Number
-      }"
-      [style.display]="name.value === '' ? 'none':'block'">
-        <h2>The Puzzle</h2>
-        <p>Ok, Welcome <span class="name">{{name.value}}</span></p>
-        <br />
-        Switch 1:
-        <input type="text" #switch1 (keyup)="0" /><br />
-        Switch 2:
-        <input type="text" #switch2 (keyup)="0" /><br />
-        Switch 3:
-        <input type="text" #switch3 (keyup)="0" /><br />
-        Switch 4:
-        <input type="text" #switch4 (keyup)="0" /><br />
-      </section>
-      <h2>Congratulations {{name.value}}, you did it!</h2>
-    `
+  templateUrl: 'dev/puzzle/puzzle.tpl.html'
 })
 
 export class PuzzleComponent implements OnInit {
