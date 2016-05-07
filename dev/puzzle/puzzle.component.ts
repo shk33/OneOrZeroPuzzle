6d9955b3e0b0304f330c4649c36c32005650a78b
Miguel Coronel <m.coronel.seg@gmail.com>
2016-05-07 02:05:21 -0500

    Add styling

--- dev/puzzle/puzzle.component.ts
+++ dev/puzzle/puzzle.component.ts
@@ -3,7 +3,25 @@ import {Component} from 'angular2/core';
 @Component({
   selector: 'my-puzzle',
   template: `
-      
+      <section class="setup">
+        <h2>Game Setup</h2>
+        Enter your name please:
+        <input type="text" />
+      </section>
+      <section>
+        <h2>The Puzzle</h2>
+        <p>Ok, Welcome <span class="name">XXX</span></p>
+        <br />
+        Switch 1:
+        <input type="text" /><br />
+        Switch 2:
+        <input type="text" /><br />
+        Switch 3:
+        <input type="text" /><br />
+        Switch 4:
+        <input type="text" /><br />
+      </section>
+      <h2>Congratulations XXX, you did it!</h2>
     `
 })
 
